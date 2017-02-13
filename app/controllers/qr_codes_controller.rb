daa46698e3adddc4242c4ba1716fd7ccbf3791a6
codeamac15 <codeamac15@gmail.com>
2017-02-13 13:32:26 -0600

    Creating and saving .png images for qrcodes

--- app/controllers/qr_codes_controller.rb
+++ app/controllers/qr_codes_controller.rb
@@ -4,7 +4,10 @@ class QrCodesController < ApplicationController
   end
 
   def create
-    @qr = RQRCode::QRCode.new( qr_code_params[:text], size: 4)
+    random = Array.new(7){[*"A".."Z", *"a".."z", *"0".."9"].sample}.join
+    @qr = RQRCode::QRCode.new( qr_code_params[:text], size: 5, :level => :h)
+    png = @qr.to_img                                             # returns an instance of ChunkyPNG
+    png.resize(90, 90).save("#{random}.png")
 
   end
 
--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,7 @@
 # require_relative 'application'
 # Load the Rails application.
 require 'rqrcode'
+require 'rqrcode_png'
 require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
